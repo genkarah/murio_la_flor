c4d8256981db086e4b700ff7d71714f91b5fb4c4
genkah <rkcortizo@gmail.com>
2024-10-11 12:04:29 -0600

    primer commit

---
+++ soyutil.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env pyhton3
+ n = int(input())
+ 
+for x in range(1,n+1):
+	print("HOLA :)")
+
---
+++ srcrive.sh
@@ -0,0 +1,7 @@
+echo "# murio_la_flor" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/genkarah/murio_la_flor.git
+git push -u origin main
